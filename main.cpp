5c5f553c4673548dda9b6836ef40de21c8f989d6
davids <davids.gregors.smits@gmail.com>
2026-01-19 11:43:15 +0200

    first commit :D

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "testh.h"
+int main(){
+    values v = values();
+    std::cout<<v.x+1/3000<<"Hello world!"<<"\n";
+}
---
+++ testh.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class values{
+public:
+int x = 1;
+
+void printx(){
+    std::cout<<x;
+}
+};
