3f3573eee1fa4f2c68f9c2787f1a97f61c6ac6b9
davids <davids.gregors.smits@gmail.com>
2026-01-27 18:46:40 +0200

    working on ceof generation for agents, plus changed health updating scheme

--- Units/agent.h
+++ Units/agent.h
@@ -4,6 +4,13 @@
 
 class Agent{
 public:
+
+float healthCoef;
+float energyCoef;
+float speedCoef;
+float sightCoef;
+
+
 float health;
 float energy;
 //color
@@ -15,11 +22,9 @@ Agent(){}
 
 Agent(float eRadiation)// default spawn
 {
-    float total_coef = 10.0;
+    generateCoefs();
 
-    health = 1;
-    energy = 1;
-    speed = 1;
+    
     brain = new Brain(eRadiation);
     // calculate color
 }
@@ -30,5 +35,22 @@ Agent(float iHealth, float iEnergy,float iSpeed, float iSight, Brain iBrain, flo
 
 
     brain = new Brain(eRadiation,iBrain);
+}
+
+void generateCoefs(){
+    float total_coef = 10.0;
+    health = static_cast <float> (rand()) / static_cast <float> (RAND_MAX)*10;
+    // rest is 10 - each calculated attribute value
+    
+}
+
+
+void updateHealth(){
+
+    health += energy*energy - 2; // health updated on negative parabola dependency of energy
+    if (health < 0){
+        health = 0;
+    }
+    
 }
 };
